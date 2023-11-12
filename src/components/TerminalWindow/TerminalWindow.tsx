2419cf1f00cb9b09f057edc3f72cca9974c382da
Silas Berger <silas.berger@outlook.com>
2023-11-12 21:48:46 +0100

    Implement first riddle.

--- src/components/TerminalWindow/TerminalWindow.tsx
+++ src/components/TerminalWindow/TerminalWindow.tsx
@@ -5,51 +5,16 @@ import {TextLine} from "../TextLine/TextLine";
 export const TerminalWindow: Component = () => {
   return (
     <div class={styles.TerminalWindow}>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
-      <TextLine/>
+      <TextLine text='Hallo.'/>
+      <TextLine text='Mein Name ist Special Agent Charles Marsh. Ich benötige dringend Ihre Hilfe.'/>
+      <TextLine text='Satellit D-128q hat heute um 04:37 UTC-0900 eine verdächtige Nachricht abgefangen.'/>
+      <TextLine text='Wir haben Grund zur Annahme, dass es sich beim Absender um den französichen Diplomaten Blaise de Vigenère handelt.'/>
+      <TextLine text='Wir haben keine Ahnung, für wen sie bestimmt ist, oder was der Inhalt sein könnte.'/>
+      <TextLine text='Die Nachricht lautet: EFPZSUOOK'/>
+      <TextLine text='Aus sicherer Quelle wissen wir, dass die Nachricht mit dem Schlüsselwort COBRA verschlüsselt wurde.'/>
+      <TextLine text='Leider kennen wir aber den verwendeten kryptografischen Algorithmus nicht.'/>
+      <TextLine text='Entschlüsseln Sie bitte die Nachricht, und geben Sie sie hier ein.'/>
+      <TextLine text='_'/>
     </div>
   );
 }
--- src/components/TextLine/TextLine.tsx
+++ src/components/TextLine/TextLine.tsx
@@ -1,11 +1,11 @@
 import {Component} from "solid-js";
 import styles from './TextLine.module.css';
 
-export const TextLine: Component = () => {
+export const TextLine = ({text}: {text: string}) => {
   return (
     <div class={styles.TextLine}>
       <div class={styles.prefix}>&gt;</div>
-      <div>This is the text. It is very long. It goes way beyond just a single line. This is important, since we need to see what happens if the line breaks.</div>
+      <div>{text}</div>
     </div>
   );
 }
