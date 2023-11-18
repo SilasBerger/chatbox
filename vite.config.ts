5fa54721a2f5380b5d1399eda0f3690164c1c386
Silas Berger <silas.berger@outlook.com>
2023-11-18 14:24:53 +0100

    Try getting port from env.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [
-    /* 
+    /*
     Uncomment the following line to enable solid-devtools.
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
     */
@@ -12,7 +12,7 @@ export default defineConfig({
     solidPlugin(),
   ],
   server: {
-    port: 3000,
+    port: import.meta.env.PORT || 3000,
   },
   build: {
     target: 'esnext',
