adf0f58b198c50feae315dc64767620298eb0dd8
Silas Berger <silas.berger@outlook.com>
2023-11-21 16:58:00 +0100

    Tweak cryptoBasics story.

--- src/games/cryptoBasics.ts
+++ src/games/cryptoBasics.ts
@@ -9,35 +9,35 @@ export const cryptoBasics: Game = {
     DialogLine.of('Wir haben keine Ahnung, für wen sie bestimmt ist, oder was der Inhalt sein könnte.'),
     DialogLine.of('Die Nachricht lautet: EFPZSUOOK'),
     DialogLine.of('Aus sicherer Quelle wissen wir, dass die Nachricht mit dem Schlüsselwort COBRA verschlüsselt wurde.'),
-    DialogLine.of('Leider kennen wir aber den verwendeten kryptografischen Algorithmus nicht.'),
+    DialogLine.of('Leider finden wir nicht heraus, welches Verschlüsselungsverfahren dazu verwendet wurde.'),
     Question.create()
       .withPrompt('Entschlüsseln Sie bitte die Nachricht.')
       .withCorrectAnswers(['CROISSANT'])
       .withAllowSubstringMatch(true)
       .withResponseToIncorrectAnswer('Das sieht mir nicht ganz richtig aus...')
-      .withResponseToCorrectAnswer('Das ist es! Vielen Dank.')
+      .withResponseToCorrectAnswer('Das ist es! Ich wusste, dass ich auf Sie zählen kann!')
       .build(),
-    DialogLine.of('Da Sie sich mit Kryptografie so gut auskennen, muss ich Sie um einen weiteren Gefallen bitten.'),
+    DialogLine.of('Haben Sie noch einen Moment Zeit für mich? Ich muss ich Sie um einen weiteren Gefallen bitten.'),
     DialogLine.of('Einer unserer besten Agenten ist in grosser Gefahr und muss dringend exfiltriert werden.'),
     DialogLine.of('SecOps-5 erwartet ihn morgen bei Sonnenaufgang am Treffpunkt KAUFHAUS.'),
     Question.create()
-      .withPrompt('Verschlüsseln Sie bitte den Namen des Treffpunkts mit dem gleichen Algorithmus und mit dem Schlüsselwort ELIXIR.')
+      .withPrompt('Verschlüsseln Sie bitte den Namen des Treffpunkts mit dem Schlüsselwort ELIXIR. Verwenden Sie ebenfalls das Vigenère verfahren.')
       .withCorrectAnswers(['OLCCPRYD'])
       .withAllowSubstringMatch(true)
       .withResponseToIncorrectAnswer('Hmm... Da scheint etwas noch nicht ganz zu stimmen.')
-      .withResponseToCorrectAnswer('Danke. Hoffentlich erinnert sich unser Agent noch an das Schlüsselwort.')
+      .withResponseToCorrectAnswer('Danke. Hoffentlich erinnert sich unser Agent noch an das Schlüsselwort ;)')
       .build(),
-    DialogLine.of('Eine letzte Bitte noch: Wir haben noch eine weitere Nachricht erhalten...'),
-    DialogLine.of('Die Nachricht lautet: NVGQZ OMZAAZI VH FJGJNNZPH'),
+    DialogLine.of('Eine letzte Bitte noch...'),
+    DialogLine.of('Ich darf Ihnen leider keine Details verraten, aber so viel sei gesagt: Wir stehen kurz vor einem denkwürdigen Zugriff.'),
+    DialogLine.of('Unsere Zielperson hat kürzlich folgende Nachricht übermittelt: NVGQZ OMZAAZI VH FJGJNNZPH'),
     DialogLine.of('Leider kennen wir weder den Verschlüsselungsmechanismus, noch den Schlüssel.'),
-    DialogLine.of('Allerdings kennen wir den Absender.'),
-    DialogLine.of('Wir wissen, dass in all seinen Nachrichten das erste Wort immer entweder AVE, SALVE oder CIAO ist.'),
+    DialogLine.of('Wir wissen aber, dass die Zielperson jede Nachricht mit AVE, SALVE oder CIAO anfängt.'),
     Question.create()
-      .withPrompt('Nutzen Sie Ihr Angriffswissen, um die geheime Nachricht zu entschlüsseln.')
+      .withPrompt('Reichen Ihnen diese Informationen, um die geheime Nachricht zu entschlüsseln?')
       .withCorrectAnswers(['SALVE TREFFEN AM KOLOSSEUM'])
       .withAllowSubstringMatch(true)
       .withResponseToIncorrectAnswer('Ich glaube das ist nicht korrekt.')
-      .withResponseToCorrectAnswer('Das ergibt Sinn! Wie der wohl reagiert, wenn am Kolosseum eine Überraschung auf ihn wartet?')
+      .withResponseToCorrectAnswer('Das macht Sinn! Ich freue mich schon auf seinen Gesichtsausdruck, wenn am Kolosseum eine nette Überraschung auf ihn wartet :D')
       .build(),
     DialogLine.of('Im Namen der Agency danke ich Ihnen für Ihre Hilfe. Passen Sie auf sich auf!'),
   ],
